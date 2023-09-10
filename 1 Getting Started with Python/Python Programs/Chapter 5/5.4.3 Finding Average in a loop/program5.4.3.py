10bef6948f2d5b2e27687bec448649aac69de260
Gaurab Kundu <gaurabkundu2003@gmail.com>
2023-09-10 17:29:38 +0530

    added print(count,sum,value) in the for loop

--- 1 Getting Started with Python/Python Programs/Chapter 5/5.4.3 Finding Average in a loop/program5.4.3.py
+++ 1 Getting Started with Python/Python Programs/Chapter 5/5.4.3 Finding Average in a loop/program5.4.3.py
@@ -5,4 +5,5 @@ print('Before' , count, sum)
 for value in [9, 41, 12, 3, 74, 15] :
     count = count +1
     sum = sum + value
-    print('After', count, sum, sum/count)
+    print(count, sum, value)
+print('After', count, sum, sum/count)
