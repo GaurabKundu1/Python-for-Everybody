f49b5a11be697c1af63c571dc473efca33a4062b
Gaurab Kundu <gaurabkundu2003@gmail.com>
2023-09-10 15:15:52 +0530

    update

--- 1 Getting Started with Python/Python Programs/Chapter 1/1.4.3 A short python story about how to count words in a file/program1.4.3.py
+++ 1 Getting Started with Python/Python Programs/Chapter 1/1.4.3 A short python story about how to count words in a file/program1.4.3.py
@@ -3,7 +3,7 @@ name = input("Enter File:")
 handle = open(name)
 counts = dict()
 for line in handle:
- words = line.split()
+    words = line.split()   
 for word in words:
     counts[word] = counts.get(word,0)+1 
 bigcount = None
